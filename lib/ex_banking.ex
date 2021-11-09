1c1be481e66c33ab0672ae56f6129b443939a71a
Anastasia M <anastasiamaschenko103@gmail.com>
2021-11-09 11:04:56 +0300

    Create user method

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -4,15 +4,25 @@ defmodule ExBanking do
   """
 
   @doc """
-  Hello world.
+  Function creates new user in the system.
+  Username must be unique string.
 
   ## Examples
 
-      iex> ExBanking.hello()
-      :world
+      iex> ExBanking.create_user("foo")
+      :ok
+
+      iex> ExBanking.create_user("bar")
+      iex> ExBanking.create_user("bar")
+      {:error, :user_already_exists}
+
+      iex> ExBanking.create_user(:bar)
+      {:error, :wrong_arguments}
 
   """
-  def hello do
-    :world
+
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(user) do
+    GenServer.call(ExBankingStack, {:create, user})
   end
 end
--- lib/ex_banking/registry.ex
+++ lib/ex_banking/registry.ex
@@ -2,12 +2,24 @@ defmodule ExBanking.Registry do
   use GenServer
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, %{})
+    GenServer.start_link(__MODULE__, %{}, name: ExBankingStack)
   end
 
   @impl true
-  def init(:ok) do
-    users = %{}
+  def init(users) do
     {:ok, users}
+  end
+
+  @impl true
+  def handle_call({:create, user}, _from, users) when is_binary(user) do
+    case users do
+      %{^user => _} -> {:reply, {:error, :user_already_exists}, users}
+      _ -> {:reply, :ok, Map.put_new(users, user, %{})}
+    end
+  end
+
+  @impl true
+  def handle_call({:create, _user}, _from, users) do
+    {:reply, {:error, :wrong_arguments}, users}
   end
 end
